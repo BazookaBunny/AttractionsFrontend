6e50c0fd39e61d2943fed257255c46c639c1552e
BazookaBunny <deadblesreaper@yahoo.com>
2025-09-18 13:40:36 +0300

    Update Url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:5263/api'
-  // apiBaseUrl: 'https://attractionsapi-b5evghdbgqbzevgr.polandcentral-01.azurewebsites.net/api'
+  // apiBaseUrl: 'http://localhost:5263/api'
+  apiBaseUrl: 'https://attractionsapi-b5evghdbgqbzevgr.polandcentral-01.azurewebsites.net/api'
 };
